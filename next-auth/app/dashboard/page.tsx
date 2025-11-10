08f8416fe3045079b7b9270fe144d45d5980cd64
seigiakatsuka <minuitspence@gmail.com>
2025-11-10 13:36:14 -0500

    added logout button

--- next-auth/app/dashboard/page.tsx
+++ next-auth/app/dashboard/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import { logout } from "../login/actions";
+
 export default function Dashboard() {
   return (
     <div>
       <h1>Dashboard</h1>
+      <button onClick={() => logout()}>Logout</button>
     </div>
   );
 }
